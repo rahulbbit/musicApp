ff4c0015ca2103ecb14bba21e3579b83ca560085
Rahul Patel <rahulpatel@rahuls-macbook-pro.local>
2015-11-19 23:36:33 +0530

    Initial Commit

---
+++ Music/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Music
+//
+//  Created by Rahul Patel on 19/11/15.
+//  Copyright © 2015 Rahul Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Music/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Music
+//
+//  Created by Rahul Patel on 19/11/15.
+//  Copyright © 2015 Rahul Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
